cc63a7d078b192b754ae612a2110f41a5ee2ff82
Natalia Kucheriava <nkucheryava@gmail.com>
2024-06-16 19:56:56 +0200

    image slader added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 // import logo from './logo.svg';
 import './App.css';
 import Accordion from './components/accordion';
+import ImageSlider from './components/image-slider';
 import RandomColorGenerator from './components/random-color-generator';
 import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
-
       {/* Accordion component */}
       <Accordion />
-      
-      {/* RandomColorGenerator component */}
+
+      {/* Random color generator component */}
       <RandomColorGenerator />
 
-      {/* StarRating component */}
-      <StarRating/>      
+      {/* Star rating component */}
+      <StarRating />
 
+      {/* Image slider component */}
+
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
